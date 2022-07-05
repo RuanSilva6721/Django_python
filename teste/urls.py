891210c87718b431924292ce730c48080ba719ac
RuanSilva2167 <ruansilva6721@gmail.com>
2022-07-05 17:45:26 -0300

    estou usando essa pasta 'teste' para fazer uns teste no django

--- teste/urls.py
+++ teste/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('helloword/', views.helloword ),
+    path('', views.testelist),
+    path('yourname/<str:name>', views.yourname)
 ]
--- teste/views.py
+++ teste/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def helloword (request):
     return HttpResponse('Hello Word!')
+
+
+def testelist(request):
+    return render(request, 'teste/list.html') 
+
+
+def yourname(request, name):
+    return render(request, 'teste/yourname.html', {'name': name} )  
