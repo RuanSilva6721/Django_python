c97efe3d7fb4b27b5b782f1d1fcc87ecb38e88d5
RuanFelipe <ruansilva6721@gmail.com>
2022-07-15 11:50:30 -0300

    criando a página de produtos

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_home, name="home"),
+    path('produto/<obj:produto>', views.comprar_produto, name="produto")
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,9 @@ from pages.models import Produto, Cliente, Venda, DetalheVenda
 def home_home(request):
     produto = Produto.objects.all()
     return render(request, 'pages/home.html', {'produto': produto})
+
+def comprar_produto(request, produto):
+    return render(request, 'pages/produto.html', {'produto':produto})
+
+# def comprar_produto(request, produto_description):
+#     return render(request, 'pages/produto.html')
