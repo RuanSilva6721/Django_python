69160161301a9f13c09c1ecd654da8bb51d5d503
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-08-01 21:18:17 +0000

    REFACT: refatorando nome de view, templates e urls

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_home, name="home"),
-    path('produto/<str:name>/<int:cod>', views.produtoDescrip, name="produto"),
-    path('newUser/', views.newUser, name="newUser")
+    path('', views.home, name="home"),
+    path('produto/<str:name>/<int:cod>', views.produto_description, name="produto"),
+    path('new-user/', views.new_user, name="new-user")
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,23 +3,22 @@ from pages.forms import ClienteForm
 from pages.models import Produto, Cliente, Venda, DetalheVenda
 
 
-def home_home(request):
+def home(request):
     produto = Produto.objects.all()
     return render(request, 'pages/home.html', {'produto': produto})
 
 
-def produtoDescrip(request, name, cod):
+def produto_description(request, name, cod):
     produtos = get_object_or_404 (Produto, pk=cod)
     if request.method == 'POST':
          produtos.qtdEstoque = produtos.qtdEstoque - 1
-         produtos.save()           
-         #return redirect('/')
+         produtos.save()
          return render (request, 'pages/produto.html', {'produtos': produtos} )
     else:
         return render (request, 'pages/produto.html', {'produtos': produtos} )
             
 
-def newUser(request):
+def new_user(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
@@ -27,4 +26,4 @@ def newUser(request):
             return redirect('/')
     else:        
         form = ClienteForm()
-        return render(request, 'pages/User.html', {'form': form})
+        return render(request, 'pages/cadastrar_user.html', {'form': form})
