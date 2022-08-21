86eb3b372f5ae1417930cf2d5e189612c95afcb3
RuanSilva2167 <ruansilva6721@gmail.com>
2022-08-21 18:01:30 -0300

    adicionado javascript

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function comprarProduto(){
+    alert("Você comprou 1 unidade!")
+}
+function entrarNoPerfil(entrar){
+    if(entrar == 1){
+        alert("falha a entrar no perfil!")
+    }
+    alert("Você entrou no perfil!")
+}
+function SairDoPerfil(){
+    alert("Você saiu no perfil!")
+}
+function cadastrarUser(ctrue){
+    if(ctrue =="true"){
+        alert("Usuário foi cadastrado com sucesso!")
+    }
+}
